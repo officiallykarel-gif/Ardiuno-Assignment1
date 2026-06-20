f95df53ed6ec2d50315a9f8ffd25992408fc0bf7
officiallykarel-gif <officiallykarel@gmail.com>
2026-06-20 18:55:36 +0000

    Add files via upload

---
+++ ASSIGNMENT1_MICHAELKAREL.ino
@@ -0,0 +1,39 @@
+int startValue = 5;
+
+int ledPin = 13;
+
+
+void flashLED(int times){
+while(times>0){
+  digitalWrite(ledPin,HIGH);
+  delay(500);
+
+  digitalWrite(ledPin,LOW);
+  delay(500);
+times--;
+  }
+}
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+pinMode(ledPin, OUTPUT);
+Serial.println("===Smart Countdown Starting===");
+
+while(startValue >= 1){
+   Serial.print("Counter : ");
+  Serial.println(startValue);   
+flashLED(startValue);
+delay(2000);
+startValue--;
+}
+Serial.println("====Countdown Complete ====");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
